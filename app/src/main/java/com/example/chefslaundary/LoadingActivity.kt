4110921d6a4ed269f919b977f5572e4108d15686
Segnit11 <98061203+Segnit11@users.noreply.github.com>
2023-05-03 20:51:34 -0500

    Update LoadingActivity.kt

--- app/src/main/java/com/example/chefslaundary/LoadingActivity.kt
+++ app/src/main/java/com/example/chefslaundary/LoadingActivity.kt
@@ -1,3 +1,12 @@
+
+/*
+* Comment generated with the assistance of Chat GPT
+* We used the help of a lot of tutorial videos to understand the Concept
+* We used the help of Stack Overflow
+* We used the help of the textBook
+* We used the help of ChatGPT especially for the errors that is happening with us
+* */
+
 package com.example.chefslaundary
 
 import android.content.Intent
@@ -7,18 +16,32 @@ import android.os.Handler
 
 class LoadingActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_loading)
+    class LoadingActivity : AppCompatActivity() {
 
-        val playerName = intent.getStringExtra("name")
+        // This method is called when the activity is created
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
 
-        Handler().postDelayed({
-            // Start main activity after 3 seconds
-            val intent = Intent(this, GameActivity::class.java)
-            intent.putExtra("playerName", playerName)
-            startActivity(intent)
-            finish()
-        }, 3000)
+            // Set the content view of the activity to the activity_loading layout
+            setContentView(R.layout.activity_loading)
+
+            // Get the string extra named "name" from the intent that started this activity and store it in a variable named playerName
+            val playerName = intent.getStringExtra("name")
+
+            // Create a new Handler object and call its postDelayed method
+            Handler().postDelayed(
+                {
+
+                    // Create a new Intent object that will start the GameActivity
+                    val intent = Intent(this, GameActivity::class.java)
+
+                    // Add the playerName variable as an extra to the intent
+                    intent.putExtra("playerName", playerName)
+
+                    // Start the GameActivity using the intent and finish this activity
+                    startActivity(intent)
+                    finish()
+                }, 3000) // Set a delay of 3 seconds before executing the code inside the Handler's postDelayed method
+        }
     }
 }
