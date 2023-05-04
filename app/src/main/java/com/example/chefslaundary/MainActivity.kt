580c1fc973f19f88c997f2d4a856e82c24a17b2b
Segnit11 <98061203+Segnit11@users.noreply.github.com>
2023-05-03 20:52:02 -0500

    Update MainActivity.kt

--- app/src/main/java/com/example/chefslaundary/MainActivity.kt
+++ app/src/main/java/com/example/chefslaundary/MainActivity.kt
@@ -1,3 +1,13 @@
+
+/*
+* Comment generated with the assistance of Chat GPT
+* We used the help of a lot of tutorial videos to understand the Concept
+* We used the help of Stack Overflow
+* We used the help of the textBook
+* We used the help of ChatGPT especially for the errors that is happening with us
+* */
+
+
 package com.example.chefslaundary
 
 import android.content.Context
@@ -11,33 +21,36 @@ import android.widget.Button
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    // Declare private properties for the MediaPlayer, volume buttons, shared preferences, and playing status
     private lateinit var mediaPlayer: MediaPlayer
     private lateinit var volumeOnButton: FloatingActionButton
     private lateinit var volumeOffButton: FloatingActionButton
-
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var editor: SharedPreferences.Editor
     private var isPlaying = false
 
-
+    // This method is called when the activity is created
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Set the content view of the activity to the activity_main layout
         setContentView(R.layout.activity_main)
 
-        // Initialize media player with background music
+        // Initialize the media player with background music and set looping and volume
         mediaPlayer = MediaPlayer.create(this, R.raw.chefmusic)
         mediaPlayer.isLooping = true
         mediaPlayer.setVolume(0.5f, 0.5f)
 
-        // Initialize volume buttons
+        // Initialize volume buttons and set their click listeners to turn music on/off
         volumeOnButton = findViewById(R.id.volume_on)
         volumeOffButton = findViewById(R.id.volume_off)
         volumeOnButton.setOnClickListener { turnOnMusic() }
         volumeOffButton.setOnClickListener { turnOffMusic() }
 
+        // Get shared preferences for music playing status and update UI accordingly
         sharedPreferences = getSharedPreferences("MUSIC", Context.MODE_PRIVATE)
         isPlaying = sharedPreferences.getBoolean("isPlaying", false)
-
         if (isPlaying) {
             mediaPlayer.start()
             volumeOnButton.visibility = View.GONE
@@ -48,6 +61,7 @@ class MainActivity : AppCompatActivity() {
         volumeOnButton.visibility = View.VISIBLE
         volumeOffButton.visibility = View.GONE
 
+        // Set click listeners for buttons to start other activities
         var newGame = findViewById<Button>(R.id.bt_Newgame)
         newGame.setOnClickListener {
             val intent = Intent(this, NameActivity::class.java)
@@ -71,34 +85,37 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent7)
         }
 
-
     }
 
-
-
+    // Turn on music and update Visibilty
     private fun turnOnMusic() {
         mediaPlayer.start()
         volumeOnButton.visibility = View.GONE
         volumeOffButton.visibility = View.VISIBLE
     }
 
+    // Turn off music and update Visibilty
     private fun turnOffMusic() {
         mediaPlayer.pause()
         volumeOnButton.visibility = View.VISIBLE
         volumeOffButton.visibility = View.GONE
     }
 
-
+    // This method is called when the activity is paused
     override fun onPause() {
         super.onPause()
+
+        // Save the music playing status in shared preferences
         editor = sharedPreferences.edit()
         editor.putBoolean("isPlaying", mediaPlayer.isPlaying)
         editor.apply()
     }
 
-
+    // This method is called when the activity is resumed
     override fun onResume() {
         super.onResume()
+
+        // Get the music playing status from shared preferences and update UI accordingly
         isPlaying = sharedPreferences.getBoolean("isPlaying", false)
         if (isPlaying) {
             volumeOnButton.visibility = View.GONE
@@ -109,10 +126,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-//    fun onNew(view: View) {
-//        val intent = Intent(this, SettingsActivity::class.java)
-//        startActivity(intent)
-//    }
 
     fun onNewGameButtonClicked(view: View){
         val intent = Intent(this, NameActivity::class.java)
