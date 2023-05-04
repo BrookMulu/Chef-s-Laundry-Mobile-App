c3c19ca948596616e72f5f6886062b0d3c6787b3
Segnit11 <98061203+Segnit11@users.noreply.github.com>
2023-05-03 20:50:11 -0500

    Update GameActivity.kt

--- app/src/main/java/com/example/chefslaundary/GameActivity.kt
+++ app/src/main/java/com/example/chefslaundary/GameActivity.kt
@@ -1,3 +1,12 @@
+
+/*
+* Comment generated with the assistance of Chat GPT
+* We used the help of a lot of tutorial videos to understand the Concept
+* We used the help of Stack Overflow
+* We used the help of the textBook
+* We used the help of ChatGPT especially for the errors that is happening with us
+* */
+
 package com.example.chefslaundary
 
 import android.content.Intent
