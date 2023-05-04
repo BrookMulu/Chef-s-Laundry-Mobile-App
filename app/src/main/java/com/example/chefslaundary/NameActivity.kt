cc5c976b81e680df9c300e51e4049a0375b1b317
Segnit11 <98061203+Segnit11@users.noreply.github.com>
2023-05-03 20:52:27 -0500

    Update NameActivity.kt

--- app/src/main/java/com/example/chefslaundary/NameActivity.kt
+++ app/src/main/java/com/example/chefslaundary/NameActivity.kt
@@ -1,3 +1,13 @@
+
+
+/*
+* Comment generated with the assistance of Chat GPT
+* We used the help of a lot of tutorial videos to understand the Concept
+* We used the help of Stack Overflow
+* We used the help of the textBook
+* We used the help of ChatGPT especially for the errors that is happening with us
+* */
+
 package com.example.chefslaundary
 
 import android.content.Intent
@@ -12,14 +22,17 @@ class NameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_name)
     }
+
+    // function to handle submit button click event
     fun onSubmitClicked(view: View) {
+        // Get the entered name from EditText view
         val name = findViewById<EditText>(R.id.etn_enterName).text.toString()
 
+        // If the entered name is empty, show a toast message and do nothing
         if (name == ""){
-
             Toast.makeText(this,"Please use a valid name.",Toast.LENGTH_SHORT).show()
-
         } else {
+            // If the entered name is valid, start the LoadingActivity and pass the name as extra data
             val intent = Intent(this, LoadingActivity::class.java)
             intent.putExtra("name", name)
             startActivity(intent)
