8e6cf910dfb9f22a058305d647966cdef981587d
Segnit11 <98061203+Segnit11@users.noreply.github.com>
2023-05-03 20:48:57 -0500

    Update AboutActivity.kt

--- app/src/main/java/com/example/chefslaundary/AboutActivity.kt
+++ app/src/main/java/com/example/chefslaundary/AboutActivity.kt
@@ -1,3 +1,14 @@
+
+
+/*
+* Comment generated with the assistance of Chat GPT
+* We used the help of a lot of tutorial videos to understand the Concept
+* We used the help of Stack Overflow
+* We used the help of the textBook
+* We used the help of ChatGPT especially for the errors that is happening with us
+* */
+
+
 package com.example.chefslaundary
 
 import android.content.Intent
@@ -8,20 +19,34 @@ import android.widget.Button
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Set the layout for the activity
         setContentView(R.layout.activity_about)
 
+        // Find the "Help" button in the layout
         val helpPage = findViewById<Button>(R.id.helpPageBtnAboutUs)
+
+        // Set an OnClickListener for the "Help" button
         helpPage.setOnClickListener{
+
+            // Create an Intent to start the FaqActivity
             val intent = Intent(this, FaqActivity::class.java)
+
+            // Start the FaqActivity
             startActivity(intent)
         }
 
-        // Find this button
+        // Find the "Home" button in the layout
         val home = findViewById<Button>(R.id.homeBtnAboutUs)
+
+        // Set an OnClickListener for the "Home" button
         home.setOnClickListener(){
+
+            // Create an Intent to start the MainActivity
             val intent = Intent(this, MainActivity::class.java)
+
+            // Start the MainActivity
             startActivity(intent)
         }
-
     }
 }
